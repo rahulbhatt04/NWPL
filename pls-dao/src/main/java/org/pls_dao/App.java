586df35b672644b473990b8c30c4e261993ef413
rbhatt <rbhatt@lendingclub.com>
2018-02-18 06:30:43 -0800

    Module for DAO

---
+++ pls-dao/src/main/java/org/pls_dao/App.java
@@ -0,0 +1,13 @@
+package org.pls_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
